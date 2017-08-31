3150ebf3840d0a6b6d6e9b639662a064d60ed378
Ruska <RRCFoxyLoxy@yandex.ru>
2017-08-31 23:57:29 +0300

    Table

--- app/src/main/java/radonsoft/mireaassistant/MainActivity.java
+++ app/src/main/java/radonsoft/mireaassistant/MainActivity.java
@@ -84,7 +84,6 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-
         int id = item.getItemId();
         FragmentTransaction ftrans = getSupportFragmentManager().beginTransaction();
         if (id == R.id.nav_schedule) {
